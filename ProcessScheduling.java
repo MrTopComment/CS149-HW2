366d10dc0ccad752e87ec6f1493b4d95b50f293e
MrTopComment <gabrielorellanaklr@gmail.com>
2015-02-19 21:46:51 -0800

    More code

--- ProcessScheduling.java
+++ ProcessScheduling.java
@@ -5,33 +5,71 @@
  */
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 public class ProcessScheduling {
-	ArrayList<Process> listOfProcesses = new ArrayList<Process>();
+	static ArrayList<Process> listOfProcesses = new ArrayList<Process>();
+	Random r = new Random();
 	
 	public static void main(String[] args)
 	{
-		
+		//parse command line = get # of processes/quantum = variable
+		int processNumber = 0;
+		for(int i= 0 ;i< processNumber ;i++)
+		{
+			float value = 0; //arrival time
+			float value2 = 0; //expected total run time
+			float value3 = 0; //completion time
+			boolean value4 = false; //flag value for first iteration
+			//randomgenerator = 2 sets of numbers
+			
+			Process tempProcess = new Process(value,value2,value3, value4);
+			listOfProcesses.add(tempProcess);
+		}
+		FCFS(listOfProcesses);
+		SJF(listOfProcesses);
+		roundRobin(listOfProcesses);
 	}
-	public void FCFS(ArrayList<Process> array)
+	
+	public static void FCFS(ArrayList<Process> array)
 	{
+		//convert array <start, stop, arrival>
+		//keeping track of the current time
+		//array [1]-->array [1]
+		//when burst time reaches 0, completion time = current time.
+		//flag for first time, if false turn true and set start time, if true ignore
 		
 	}
-	public void SJF(ArrayList<Process> array)
+	public static void SJF(ArrayList<Process> array)
 	{
-		
+		//convert array <start, stop, arrival>
+		//keeping track of the current time
+		//array [1]-->array [1]
+		//alogirthm for SJF-->1 and then 
+		//when burst time reaches 0, completion time = current time.
+		//flag for first time, if false turn true and set start time, if true ignore
 	}
-	public void RoundRobin(ArrayList<Process> array)
+	public static void roundRobin(ArrayList<Process> array)
 	{
-		
+		//convert array <start, stop, arrival>
+		//keeping track of the current time
+		//array [1]-->array [1] also moves to back, fk i don't know how to do it
+		//
+		//when burst time reaches 0, completion time = current time.
+		//flag for first time, if false turn true and set start time, if true ignore
 	}
-	public class Process
+	public static class Process
 	{
 		private float arrivalTime;
 		private float expectedTotalRunTime;
-		Process(float arrivalTime, float burstTime)
+		private float completionTime;
+		private boolean firstIteration = false; //just in case
+		Process(float arrivalTime, float expectedTotalRunTime, float completionTime, boolean firstIteration)
 		{
 			this.arrivalTime = arrivalTime;
-			this.expectedTotalRunTime = burstTime;
+			this.expectedTotalRunTime = expectedTotalRunTime;
+			this.completionTime = completionTime;
+			this.firstIteration = firstIteration;
 		}
+		
 	}
 }
