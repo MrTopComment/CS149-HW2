d27a1494615be66e45277819876c967db484bab8
inifist <inifist@gmail.com>
2015-02-21 21:39:56 -0800

    FCFS DEEP COPY ARRAYlist urghhhhh
    I NEEDA DEEP COPY THAT ARRAYLIST
    I THINK JAVA 8 STREAM STUFF HAS SOMETHING ON THAT

--- ProcessScheduling.java
+++ ProcessScheduling.java
@@ -3,12 +3,11 @@
  * CS 149, Section 6
  * Homework 2 - Part 2
  */
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Random;
 public class ProcessScheduling 
 {
-    static ArrayList<Process> listOfProcesses = new ArrayList<Process>();
+    private static int processNumber;
+    private static ArrayList<Process> listOfProcesses = new ArrayList<Process>();
 
     public static void main(String[] args)
     {
@@ -16,9 +15,8 @@ public class ProcessScheduling
          * @processNumber, number of processes, can be 50 or 100, technically positive int values
          * @quantaRR,time slice, can be 1 -2 quanta of time
          */
-        float[] stringTo_int = new float[args.length];
-        float processNumber = Float.parseFloat(args[0]);
-        float quantaRR = Float.parseFloat(args[1]);
+        processNumber = Integer.parseInt(args[0]);
+        int quantaRR = Integer.parseInt(args[1]);
         
         //make processes
         for(int i= 0 ;i< processNumber ;i++)
@@ -32,14 +30,16 @@ public class ProcessScheduling
             /* random generator-->arrival time, expected total run time
              * @arrival time, float from 0 - 200 quanta
              * @ expected total run time, float from 0.1 - 10 quanta
+             * one instance of each to map to each process
              */
-            Random r = new Random();
-            //have an equally random chance to choose between values 0-200
-            //have an equally random chance to choose between values 0.1 - 10
-            //increments of 0.1
-            //here, the random arrival time and completion time will be added to process
+            float random1 = (Math.abs((int) System.nanoTime())%101); 
+            random1 = (random1/100)*200;
+            float random2 = (Math.abs((int) System.nanoTime())%101); 
+            random2 = (random2/100)*10;
+            value = random1;
+            value2 = random2;
             
-            Process tempProcess = new Process(value,value2,value3,value4, value5);
+            Process tempProcess = new Process(value,value2,value3,value4,value5);
             listOfProcesses.add(tempProcess);
         }
         FCFS(listOfProcesses);
@@ -47,15 +47,31 @@ public class ProcessScheduling
         roundRobin(listOfProcesses, quantaRR); //takes in time slice 
     }
 
-    public static void FCFS(ArrayList<Process> array)
+    public static void FCFS(ArrayList<Process> arrayOG)
     {
         /* input@array <arrival time, expected total run time, completion time, 
          *              first iteration flag, first iteration time>
          */
+        //make a SORTED copy for nondestructive editing
+        ArrayList<Process> tempSort = new ArrayList<Process>(arrayOG);
         //keeping track of the current time
         float currentTimeFCFS = 0;
-        //iterate through the new array---use currentTime as the time
-        for (int i = 0; i < array.size() ; i++)
+        //sort array by arrival time --> tempSort = sorted queue
+        for (int k = 1; k < tempSort.size(); k++)
+        {
+            //insertion sort
+            float temp = tempSort.get(k).arrivalTime;
+            int m;
+            for (m = k - 1; m >= 0 && temp < tempSort.get(m).arrivalTime; m--)
+            {
+                tempSort.get(m + 1).arrivalTime = tempSort.get(m).arrivalTime;
+                tempSort.get(m + 1).arrivalTime = temp;
+            }
+        }
+        ArrayList<Process> array = new ArrayList<Process>(tempSort); //array = editable queue
+        //iterate through the new array---(FCFS logic for scheduling)
+        //no CPU scheduler switching necessary for FCFS  
+        for (int i = 0; i < array.size(); i++)
         {
             //check first iteration flag-->if false-->true and set start time--> else if true - ignore
             if (array.get(i).firstIteration == false)
@@ -63,82 +79,50 @@ public class ProcessScheduling
                 array.get(i).firstIteration = true;
                 array.get(i).executionTime = currentTimeFCFS; //first iteration time = currentTime
             }
-            //completion time = current time + burst time -->set burst to zero (FCFS logic)
+            //set current time to @completion time for process = current time + burst time
+            //set completion time = current time(already set as completion time), set burst to zero
+            currentTimeFCFS = currentTimeFCFS + array.get(i).expectedTotalRunTime;
             array.get(i).completionTime = currentTimeFCFS + array.get(i).expectedTotalRunTime;
             array.get(i).expectedTotalRunTime = 0;
-            //when to exit-->if last value has burst time zero [for FCFS, exits when array is traversed]
+            //set each burst time to 0, tempSort still holds info though
+            //non-preemptive = no need to exit.
         }
         
         //go through array for calculations and do averages [average variables] then print
         float averageTurnAroundTimeFCFS = 0;
         float averageWaitingTimeFCFS = 0;
         float averageResponseTimeFCFS = 0;
+        float throughPutFCFS = 0;
         for (int j = 0; j < array.size(); j++)
         {
+            System.out.println(tempSort.get(j).expectedTotalRunTime);
             //calculate times and then add them to the total
-            averageTurnAroundTimeFCFS = averageTurnAroundTimeFCFS + 
+            averageTurnAroundTimeFCFS = averageTurnAroundTimeFCFS + //turnaround = completion - arrival
                     ( (array.get(j).completionTime) - (array.get(j).arrivalTime) ); 
-            averageWaitingTimeFCFS = averageWaitingTimeFCFS + 
-                    ( () - () );
-            averageResponseTimeFCFS = averageResponseTimeFCFS + 
+            averageWaitingTimeFCFS = averageWaitingTimeFCFS + //wait = completion - OG array(burst time)
+                    ( (array.get(j).completionTime) - (tempSort.get(j).expectedTotalRunTime) );
+            averageResponseTimeFCFS = averageResponseTimeFCFS + //reponse = execution - arrival
                     ( (array.get(j).executionTime) - (array.get(j).arrivalTime) );
+            //throughput = processes / total time [here = adding up the burst time total]
+            throughPutFCFS = throughPutFCFS + (tempSort.get(j).expectedTotalRunTime);
         }
         //divide total by size for averages
-        averageTurnAroundTimeFCFS = averageTurnAroundTimeFCFS / array.size();
-        averageWaitingTimeFCFS = averageWaitingTimeFCFS / array.size();
-        averageResponseTimeFCFS = averageResponseTimeFCFS / array.size();
-        System.out.println(averageTurnAroundTimeFCFS);
-        System.out.println(averageWaitingTimeFCFS);
-        System.out.println(averageResponseTimeFCFS);
+        averageTurnAroundTimeFCFS = averageTurnAroundTimeFCFS / processNumber;
+        averageWaitingTimeFCFS = averageWaitingTimeFCFS / processNumber;
+        averageResponseTimeFCFS = averageResponseTimeFCFS / processNumber;
+        throughPutFCFS = processNumber / throughPutFCFS;
+        System.out.println("Average turnaround time FCFS: " + averageTurnAroundTimeFCFS);
+        System.out.println("Average waiting time FCFS: " + averageWaitingTimeFCFS);
+        System.out.println("Average reponse time FCFS: " + averageResponseTimeFCFS);
+        System.out.println("Average throughput FCFS: " + throughPutFCFS);
     }
     
-    public static void SJF(ArrayList<Process> array)
+    public static void SJF(ArrayList<Process> arrayOG)
     {
-        /* input@array <arrival time, expected total run time, completion time, 
-         *              first iteration flag, first iteration time>
-         */
-        //keeping track of the current time
-        float currentTimeSJF = 0;
-        //iterate through the new array---use currentTime as the time
-        for (int i = 0; i < array.size() ; i++)
-        {
-            //check first iteration flag-->if false-->true and set start time--> else if true - ignore
-            if (array.get(i).firstIteration == false)
-            {
-                array.get(i).firstIteration = true;
-                array.get(i).executionTime = currentTimeSJF; //first iteration time = currentTime
-            }
-            //completion time = current time + burst time -->set burst to zero (SJF logic)
-            array.get(i).completionTime = currentTimeSJF + array.get(i).expectedTotalRunTime;
-            array.get(i).expectedTotalRunTime = 0;
-            //need to do ordering logic 
-            //when to exit-->if last value has burst time zero [for SJF, ]
-        }
         
-        //go through array for calculations and do averages [average variables] then print
-        float averageTurnAroundTimeSJF = 0;
-        float averageWaitingTimeSJF = 0;
-        float averageResponseTimeSJF = 0;
-        for (int j = 0; j < array.size(); j++)
-        {
-            //calculate times and then add them to the total
-            averageTurnAroundTimeSJF = averageTurnAroundTimeSJF + 
-                    ( (array.get(j).completionTime) - (array.get(j).arrivalTime) ); 
-            averageWaitingTimeSJF = averageWaitingTimeSJF + 
-                    ( () - () );
-            averageResponseTimeSJF = averageResponseTimeSJF + 
-                    ( (array.get(j).executionTime) - (array.get(j).arrivalTime) );
-        }
-        //divide total by size for averages
-        averageTurnAroundTimeSJF = averageTurnAroundTimeSJF / array.size();
-        averageWaitingTimeSJF = averageWaitingTimeSJF / array.size();
-        averageResponseTimeSJF = averageResponseTimeSJF / array.size();
-        System.out.println(averageTurnAroundTimeSJF);
-        System.out.println(averageWaitingTimeSJF);
-        System.out.println(averageResponseTimeSJF);
     }
     
-    public static void roundRobin(ArrayList<Process> array, float timeSlice)
+    public static void roundRobin(ArrayList<Process> arrayOG, float timeSlice)
     {
         //convert array <start, stop, arrival>
         //keeping track of the current time
